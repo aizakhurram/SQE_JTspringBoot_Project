4acd3478b81deea41dbb68430ee50ab2c5192525
minaalkhurram <minaalkhurram318@gmail.com>
2023-12-02 00:15:16 +0500

    test case for checking admin home page button mapping

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -1,13 +1,51 @@
 package com.jtspringproject.JtSpringProject;
 
+import com.jtspringproject.JtSpringProject.controller.AdminController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class JtSpringProjectApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
+	}*/
+	@Mock
+	private Model model;
+
+	@InjectMocks
+	private AdminController adminController;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
 	}
 
+	@Test
+	void testAdminHome_AdminLoggedIn() {
+		adminController.adminlogcheck=1;
+		when(model.addAttribute("username", "admin")).thenReturn(model); // Assuming admin is the username
+
+		String result = adminController.adminHome(model);
+
+		assertEquals("adminHome", result);
+	}
+
+	@Test
+	void testAdminHome_AdminNotLoggedIn() {
+		adminController.adminlogcheck=0;
+
+		String result = adminController.adminHome(model);
+
+		assertEquals("redirect:/admin/login", result);
+	}
 }
