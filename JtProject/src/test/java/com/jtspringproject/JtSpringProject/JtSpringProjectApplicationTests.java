445be1114a639e16de29ff3ff2c863cc3c4e523d
minaalkhurram <minaalkhurram318@gmail.com>
2023-12-02 01:43:32 +0500

    test case for checking logout feature for user

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -1,6 +1,9 @@
 package com.jtspringproject.JtSpringProject;
 
 import com.jtspringproject.JtSpringProject.controller.AdminController;
+import com.jtspringproject.JtSpringProject.dao.productDao;
+import com.jtspringproject.JtSpringProject.models.Category;
+import com.jtspringproject.JtSpringProject.models.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,5 +50,11 @@ class JtSpringProjectApplicationTests {
 		String result = adminController.adminHome(model);
 
 		assertEquals("redirect:/admin/login", result);
+	}
+
+	@Test
+	void testReturnIndex() {
+		String result = adminController.returnIndex();
+		assertEquals("userLogin", result);
 	}
 }
