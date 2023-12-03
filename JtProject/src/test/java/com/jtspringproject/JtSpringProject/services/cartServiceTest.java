b12d0d89f5450f6f8d06dc543d863e9a74d8dc19
Tayyab Nadeem <103959510+TayyabNadeem1@users.noreply.github.com>
2023-12-03 23:31:09 +0500

    Update cartServiceTest.java

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/services/cartServiceTest.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/services/cartServiceTest.java
@@ -1,11 +1,18 @@
 package com.jtspringproject.JtSpringProject.services;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.jtspringproject.JtSpringProject.dao.cartDao;
 import com.jtspringproject.JtSpringProject.models.Cart;
 import com.jtspringproject.JtSpringProject.models.User;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -14,93 +21,107 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ContextConfiguration(
-        classes = {cartService.class}
-)
-@ExtendWith({SpringExtension.class})
-class cartServiceTest {
+@ContextConfiguration(classes = {cartService.class})
+@ExtendWith(SpringExtension.class)
+public class cartServiceTest {
     @MockBean
     private cartDao cartDao;
+
     @Autowired
     private cartService cartService;
 
-    cartServiceTest() {
-    }
-
+    /**
+     * Method under test: {@link cartService#addCart(Cart)}
+     */
     @Test
     void testAddCart() {
         User customer = new User();
         customer.setAddress("42 Main St");
-        customer.setEmail("abc@gmail.com");
+        customer.setEmail("jane.doe@example.org");
         customer.setId(1);
-        customer.setPassword("abc");
+        customer.setPassword("iloveyou");
         customer.setRole("Role");
         customer.setUsername("janedoe");
+
         Cart cart = new Cart();
         cart.setCustomer(customer);
         cart.setId(1);
-        cart.setProducts(new ArrayList());
-        Mockito.when(this.cartDao.addCart((Cart)Mockito.any())).thenReturn(cart);
+        cart.setProducts(new ArrayList<>());
+        when(cartDao.addCart(Mockito.<Cart>any())).thenReturn(cart);
+
         User customer2 = new User();
         customer2.setAddress("42 Main St");
-        customer2.setEmail("abc@gmail.com");
+        customer2.setEmail("jane.doe@example.org");
         customer2.setId(1);
-        customer2.setPassword("pass");
+        customer2.setPassword("iloveyou");
         customer2.setRole("Role");
         customer2.setUsername("janedoe");
+
         Cart cart2 = new Cart();
         cart2.setCustomer(customer2);
         cart2.setId(1);
-        cart2.setProducts(new ArrayList());
-        Cart actualAddCartResult = this.cartService.addCart(cart2);
-        ((cartDao)Mockito.verify(this.cartDao)).addCart((Cart)Mockito.any());
-        Assertions.assertSame(cart, actualAddCartResult);
+        cart2.setProducts(new ArrayList<>());
+        Cart actualAddCartResult = cartService.addCart(cart2);
+        verify(cartDao).addCart(Mockito.<Cart>any());
+        assertSame(cart, actualAddCartResult);
     }
 
-
-
+    /**
+     * Method under test: {@link cartService#getCarts()}
+     */
     @Test
     void testGetCarts() {
-        ArrayList<Cart> cartList = new ArrayList();
-        Mockito.when(this.cartDao.getCarts()).thenReturn(cartList);
-        List<Cart> actualCarts = this.cartService.getCarts();
-        ((cartDao)Mockito.verify(this.cartDao)).getCarts();
-        Assertions.assertTrue(actualCarts.isEmpty());
-        Assertions.assertSame(cartList, actualCarts);
+        ArrayList<Cart> cartList = new ArrayList<>();
+        when(cartDao.getCarts()).thenReturn(cartList);
+        List<Cart> actualCarts = cartService.getCarts();
+        verify(cartDao).getCarts();
+        assertTrue(actualCarts.isEmpty());
+        assertSame(cartList, actualCarts);
     }
 
+    /**
+     * Method under test: {@link cartService#updateCart(Cart)}
+     */
     @Test
     void testUpdateCart() {
-        ((cartDao)Mockito.doNothing().when(this.cartDao)).updateCart((Cart)Mockito.any());
+        doNothing().when(cartDao).updateCart(Mockito.<Cart>any());
+
         User customer = new User();
         customer.setAddress("42 Main St");
-        customer.setEmail("abc@gmail.com");
+        customer.setEmail("jane.doe@example.org");
         customer.setId(1);
-        customer.setPassword("hello");
+        customer.setPassword("iloveyou");
         customer.setRole("Role");
-        customer.setUsername("abc");
+        customer.setUsername("janedoe");
+
         Cart cart = new Cart();
         cart.setCustomer(customer);
         cart.setId(1);
-        cart.setProducts(new ArrayList());
-        this.cartService.updateCart(cart);
-        ((cartDao)Mockito.verify(this.cartDao)).updateCart((Cart)Mockito.any());
+        cart.setProducts(new ArrayList<>());
+        cartService.updateCart(cart);
+        verify(cartDao).updateCart(Mockito.<Cart>any());
     }
 
+    /**
+     * Method under test: {@link cartService#deleteCart(Cart)}
+     */
     @Test
     void testDeleteCart() {
-        ((cartDao)Mockito.doNothing().when(this.cartDao)).deleteCart((Cart)Mockito.any());
+        doNothing().when(cartDao).deleteCart(Mockito.<Cart>any());
+
         User customer = new User();
         customer.setAddress("42 Main St");
-        customer.setEmail("abc@gmail.com");
+        customer.setEmail("jane.doe@example.org");
         customer.setId(1);
-        customer.setPassword("hello");
+        customer.setPassword("iloveyou");
         customer.setRole("Role");
-        customer.setUsername("abc");
+        customer.setUsername("janedoe");
+
         Cart cart = new Cart();
         cart.setCustomer(customer);
         cart.setId(1);
-        cart.setProducts(new ArrayList());
-        this.cartService.deleteCart(cart);
-        ((cartDao)Mockito.verify(this.cartDao)).deleteCart((Cart)Mockito.any());
+        cart.setProducts(new ArrayList<>());
+        cartService.deleteCart(cart);
+        verify(cartDao).deleteCart(Mockito.<Cart>any());
     }
+}
