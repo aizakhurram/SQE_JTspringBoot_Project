2f7ab9c9317e5c18d701e077e843af8269d12ee9
minaalkhurram <minaalkhurram318@gmail.com>
2023-12-03 18:48:52 +0500

    test case - testing context loads (admin controller )

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -10,6 +10,7 @@ import com.jtspringproject.JtSpringProject.models.User;
 import com.jtspringproject.JtSpringProject.services.userService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.ModelAndView;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -26,12 +28,13 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 class JtSpringProjectApplicationTests {
 
-    @Mock
-    private SpringApplication springApplicationMock;
+	@Autowired
+	private AdminController controller;
+
 
 	@Test
-	void contextLoads() {
-
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 
