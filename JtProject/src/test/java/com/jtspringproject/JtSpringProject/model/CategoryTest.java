6b07b2a587726ea4c257886d10d9c1fe73ec5f9b
Tayyab Nadeem <103959510+TayyabNadeem1@users.noreply.github.com>
2023-12-03 23:47:01 +0500

    Update CategoryTest.java

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/model/CategoryTest.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/model/CategoryTest.java
@@ -1,29 +1,27 @@
-package com.jtspringproject.JtSpringProject.model;
+package com.jtspringproject.JtSpringProject.models;
 
-import com.jtspringproject.JtSpringProject.models.Category;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-class CategoryTest {
-    CategoryTest() {
-    }
-
+public class CategoryTest {
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>{@link Category#setId(int)}
+     *   <li>{@link Category#setName(String)}
+     *   <li>{@link Category#getId()}
+     *   <li>{@link Category#getName()}
+     * </ul>
+     */
     @Test
     void testSetId() {
         Category category = new Category();
         category.setId(1);
         category.setName("Name");
         int actualId = category.getId();
-        Assertions.assertEquals("Name", category.getName());
-        Assertions.assertEquals(1, actualId);
-    }
-     @Test
-    void testSetName() {
-        Category category = new Category();
-        category.setId(1);
-        category.setName("Name");
-        String actualName=category.getName();
-        Assertions.assertEquals("Name",actualName);
-        Assertions.assertEquals(1, category.getId());
+        assertEquals("Name", category.getName());
+        assertEquals(1, actualId);
     }
 }
