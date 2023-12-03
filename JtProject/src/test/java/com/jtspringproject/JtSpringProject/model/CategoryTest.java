8adb5bf77d7ed45a59738f0b5d19e0d8ab9d687c
Aiza Khurram <102323528+aizakhurram@users.noreply.github.com>
2023-12-03 21:12:07 +0500

    Add files via upload

---
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/model/CategoryTest.java
@@ -0,0 +1,25 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.jtspringproject.JtSpringProject.model;
+
+import com.jtspringproject.JtSpringProject.models.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CategoryTest {
+    CategoryTest() {
+    }
+
+    @Test
+    void testSetId() {
+        Category category = new Category();
+        category.setId(1);
+        category.setName("Name");
+        int actualId = category.getId();
+        Assertions.assertEquals("Name", category.getName());
+        Assertions.assertEquals(1, actualId);
+    }
+}
