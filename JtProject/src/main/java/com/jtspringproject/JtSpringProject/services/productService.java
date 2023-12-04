75f60d220bfd6aa5f3b25ade6c29f1c9ae0347e5
Aiza Khurram <102323528+aizakhurram@users.noreply.github.com>
2023-12-04 23:00:42 +0500

    Update productService.java (imported a library to fix the workflow)

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/services/productService.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/services/productService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jtspringproject.JtSpringProject.dao.productDao;
 import com.jtspringproject.JtSpringProject.models.Product;
+import javax.transaction.Transactional;
 
 @Service
 public class productService {
